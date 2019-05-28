11a8c718a7da11aaf926bf8d6699137ca6a11ed6
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-05-28 18:31:48 +0200

    added basic styles

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Aplikacja Licznika w ReactJS</h1>
       </header>
+      <main className="Counter-main">
+
+          <h2 className="Counter-display">
+            Licznik: <span className="Counter-display--number">15</span>
+          </h2>
+          <div className="Button-panel"> 
+              <button className="btn">Add 1</button>
+              <button className="btn">ReInit</button>
+              <button className="btn">Reset</button>
+          </div>
+          <div className="Counter-step">
+            <span className="Step-label">Krok:</span>
+            <input className="Step-input" type="number"/>
+          </div>
+
+          
+      </main>
     </div>
   );
 }
