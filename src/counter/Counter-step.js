325bccac40b56dc9b094c2280e645719c1a23576
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-05-29 20:25:32 +0200

    improved functionality by adding on change

--- src/counter/Counter-step.js
+++ src/counter/Counter-step.js
@@ -8,7 +8,7 @@ class CounterStep extends Component{
             <div className="Counter-step">
                 <h1>test</h1>
                 <span className="Step-label">Krok:</span>
-                <input onClick={()=>(this.props.getInputValue(this.step.value))} defaultValue="1" ref={(c) => this.step = c} className="Step-input" type="number"/>
+                <input onChange={()=>(this.props.getInputValue(this.step.value))} defaultValue="1" ref={(c) => this.step = c} className="Step-input" type="number"/>
 
                 <button onClick={this.props.addSteps}  className="btn">Add {this.props.stepValue}</button>
             </div>
